581dbdd95902f960e1c3a9e62524d8f27f168438
Md. Shaeakh Ahmed Chowdhury <102303488+shaeakh@users.noreply.github.com>
2022-10-21 18:32:07 +0100

    Create 65. A. Cowardly Rooks.cpp

---
+++ 65. A. Cowardly Rooks.cpp
@@ -0,0 +1,20 @@
+/*bismillahir~rahmanir~rahim*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t,m,n,x,y;
+    cin>>t;
+    while(t--){
+        cin>>n>>m;
+        for(int i=0;i<m;i++){ cin>>x>>y; }
+        if(m>=n){ cout<<"NO"<<endl; }
+        else{ cout<<"YES"<<endl; }
+
+    }
+
+    return 0;
+}
+
+/* problem link: https://codeforces.com/contest/1749/problem/A*/
+
