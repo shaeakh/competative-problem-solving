e9971eaeb462edded45938b2ec37f5ca49ad9740
Md. Shaeakh Ahmed Chowdhury <shaeakh12@gmail.com>
2023-03-01 16:43:26 +0600

    1765E

---
+++ E_Exchange.cpp
@@ -0,0 +1,18 @@
+/*bismillahir~rahmanir~rahim*/
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long int n,a,b;
+        cin>>n>>a>>b;
+        if(a>b) cout<<"1\n";
+        else{
+            if(n%a==0) cout<<n/a<<"\n";
+            else cout<< n/a+1 <<"\n";
+        }
+    }
+    return 0;
+}
+/* problem link: */
