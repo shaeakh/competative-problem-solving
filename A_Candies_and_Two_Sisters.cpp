2cce8ee6b602ed05255444aca9baccbb16e534c6
Md. Shaeakh Ahmed Chowdhury <102303488+shaeakh@users.noreply.github.com>
2023-04-02 19:16:27 +0600

    Add files via upload

---
+++ A_Candies_and_Two_Sisters.cpp
@@ -0,0 +1,25 @@
+/*bismillahir~rahmanir~rahim*/
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        long long int n,x;
+        cin>>n;
+        x = floor ( ( (n-1) * 1.0 ) / 2.0  );
+        cout<<x<<endl;
+
+    }
+    
+}
+
+int main(){
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    solve();
+    return 0;
+}
+/* problem link: https://codeforces.com/problemset/problem/1335/A */
